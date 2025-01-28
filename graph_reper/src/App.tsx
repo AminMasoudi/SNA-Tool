0c93aae95023376384e6e09ba72f27276c288e86
amin masoudi <aminmasoudi2003@gmail.com>
2025-01-29 00:08:09 +0330

    Add Chackra + Grids + darkmode

--- graph_reper/src/App.tsx
+++ graph_reper/src/App.tsx
@@ -1,12 +1,34 @@
-import './App.css'
-import ForceDirectedGraph from './ForceDirectedGraph'
-function App() {
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+import ForceDirectedGraph from "./ForceDirectedGraph";
 
+function App() {
   return (
-    <div className="App">
-      <ForceDirectedGraph />
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"left-sidebar main right-sidebar"`,
+      }}
+    >
+      <Show above="lg">
+        <GridItem area={"left-sidebar"} bg={"coral"}>
+          Left
+        </GridItem>
+      </Show>
+      
+      <GridItem area={"main"}>
+  {/* <div className="App"> */}
+    <ForceDirectedGraph />
+  {/* </div> */}
+
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"right-sidebar"} bg={"gold"}>
+          Right
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
