8b7d3083414a00dab6b27c7eff77521fbe96dd5d
amin masoudi <aminmasoudi2003@gmail.com>
2025-01-30 18:50:35 +0330

    update Main component

--- graph_reper/src/components/Main/Main.tsx
+++ graph_reper/src/components/Main/Main.tsx
@@ -1,44 +1,45 @@
-import {
-    Box,
-    Center,
-    Heading,
-    Spinner,
-  } from "@chakra-ui/react";
-  import useNodes from '../../hooks/useNodes'
-  import useEdges from "../../hooks/useEdges";
-  import { useRef } from "react";
-  import ForceDirectedGraph from "../../ForceDirectedGraph";
-  
-  function Main() {
-    const { nodes, error: nodeError, loading: nodeLoading } = useNodes();
-    const { edges, error: edgeError, loading: edgeLoading } = useEdges();
-    const svgRef = useRef<SVGSVGElement | null>(null);
-  
-    return (
-      <>
-        <Box paddingX="10px" height={window.innerHeight * 0.9}>
-          <Box>
-            <Heading>Graph View</Heading>
-          </Box>
-          <Box padding={0}>
-            {nodeLoading && edgeLoading ? (
-              <Center h="calc(100vh - 64px)" marginX="auto">
+import { Alert, Box, Center, Heading, Spinner } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import Graph from "./Graph";
+import { Node } from "../../services/node-service";
+
+interface MainProps{
+  setSelectedNode: (d: Node) => void;
+}
+
+function Main({setSelectedNode}: MainProps) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const svgRef = useRef<SVGSVGElement | null>(null);
+
+  return (
+    <>
+      <Box paddingX="10px" height={window.innerHeight * 0.9}>
+        <Box>
+          <Heading>Graph View</Heading>
+        </Box>
+        <Box padding={0}>
+          {error && (
+            <Alert variant="solid" status="error">
+              Error fetching data
+            </Alert>
+          )}
+          {loading && (
+            <Center h="calc(100vh - 64px)" marginX="auto">
               <Spinner
                 thickness="4px"
                 speed="0.65s"
                 emptyColor="gray.200"
                 color="blue.500"
                 size="xl"
-                />
-                </Center>
-            ) : (
-              <ForceDirectedGraph />
-            )}
-          </Box>
+              />
+            </Center>
+          )}
+          <Graph setSelectedNode={setSelectedNode} setError={setError} setLoading={setLoading} />
         </Box>
-      </>
-    );
-  }
-  
-  export default Main;
-  
+      </Box>
+    </>
+  );
+}
+
+export default Main;
